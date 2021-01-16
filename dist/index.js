4d7be2f3baae456f8533c4319a27f99652713614
Alexis Colin <nesquimo@gmail.com>
2021-01-16 15:19:22 +0100

    Try fixing zoom picture

--- dist/index.js
+++ dist/index.js
@@ -46666,7 +46666,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59718" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "61789" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
