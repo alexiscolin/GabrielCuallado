c8aca84210f70267e538bfb63dea292f8f5a7375
Alexis Colin <nesquimo@gmail.com>
2021-05-08 12:48:47 +0200

    Add animation on contact page

--- site/assets/js/router/contact.js
+++ site/assets/js/router/contact.js
@@ -24,11 +24,18 @@ const contactTransition = {
         const canvas = document.querySelector('.dom-gl');
         gsap.to(canvas, {autoAlpha: 1, duration: .5});
         gsap.from(next.container, {autoAlpha: 0, duration: 1})
+        gsap.to('.js-contact-anim', {autoAlpha: 1, stagger: .1})
+
     },
     once({ current, next, trigger }) {
         const canvas = document.querySelector('.dom-gl');
         gsap.to(canvas, {autoAlpha: 1, duration: .5});
         gsap.from(next.container, {autoAlpha: 0, duration: 1})
+        gsap.to('.js-contact-anim', {autoAlpha: 1, duration: 1.4, stagger: .12})
+        gsap.to('.js-contact', {"--rotate-contact": "rotate(14deg)", duration: 1.2});
+        gsap.to('.js-contact', {"--scale-contact": "scaleY(1)", duration: 1.2});
+
+
     },
     to: {
         namespace: [
